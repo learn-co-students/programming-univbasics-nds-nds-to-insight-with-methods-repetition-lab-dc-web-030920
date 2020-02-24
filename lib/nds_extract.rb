5265785f74248d53758f210cead42a04bf2816f7
Beza Sirak <sirak.bezawit@gmail.com>
2020-02-24 04:02:36 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,11 +4,14 @@ require_relative './directors_database'
 def directors_totals(source)
   result = {}
   director_index = 0
+  # pp  source
   while director_index < source.size do
     director = source[director_index]
+    # pp director
     result[director[:name]] = gross_for_director(director)
     director_index += 1
   end
+  # pp result
   result
 end
 
@@ -20,11 +23,21 @@ def gross_for_director(d)
     total += d[:movies][index][:worldwide_gross]
     index += 1
   end
-
+  # puts total
   total
 end
 
 def list_of_directors(source)
+  # pp source.length
+  directors = []
+  
+  source.each { |block|
+    pp block
+    directors << block[:name]
+  }
+  
+  directors
+  
   # Write this implementation
 end
 
@@ -38,6 +51,16 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  
+  result_hash = directors_totals(source)
+  
+  directors = list_of_directors(source)
+  total_gross = 0 
+  directors.each { |director|
+    total_gross += result_hash[director]
+  }
+  total_gross
+  
 end
 
 
